3fe3f6c5aa76255c5a675dafcc9c6305f10fbf51
dylanmezach <60929171+dylanmezach@users.noreply.github.com>
2021-10-14 15:35:24 +0200

    first commit

---
+++ DE_analysis.R
@@ -0,0 +1,194 @@
+#### Differential gene expression analysis ###
+# This script will perform a differential expression analysis of 2 different conditions.
+# It will also produce a manhattenplot, highlighting genes targeted by a transcription
+# factor of wich the motif is encriched in the DE genese 
+
+### Required packages ###
+library(DESeq2)
+library(ggplot2)
+library(ggsci)
+library(ggrepel)
+library(RColorBrewer)
+library(pheatmap)
+library(gridExtra)
+library(biomaRt)
+library(qqman)
+library(dplyr)
+library(GLAD)
+
+# Import data #
+mRNA_data <- read.table('g:/Mijn Drive/FromBigDataToPersonalizedMedicine/Data/BMS_practicum_okt2021.expression.genelevel.v75.htseq.txt', header = T, row.names = 1)
+
+# Use the information in the column names to generate a data.frame comprising the sample information.
+colData <- data.frame(row.names = colnames(mRNA_data), 
+                      sample= unlist(lapply(colnames(mRNA_data), 
+                                            function(x){unlist(strsplit(x, split = "_"))[1]})), 
+                      stimulation= unlist(lapply(colnames(mRNA_data), 
+                                                 function(x){unlist(strsplit(x, split = "_"))[2]})))
+
+# Add library size, which is the total ammount of gene reads per sample
+colData$libSize <- colSums(mRNA_data)
+#First 10 rowns of the colData object
+head(colData)
+
+####### PCA #######
+
+dds <- DESeqDataSetFromMatrix(countData = mRNA_data,
+                              colData = colData,
+                              design = ~ sample)
+
+vst <- assay(vst(dds, blind=FALSE))
+# To calculate the components by sample we need to transpose our matrix of normalized gene expression 
+pcData <- prcomp(t(vst))
+pcVar <- summary(pcData)
+
+varPC1 <- pcVar$importance[2,1]
+varPC2 <- pcVar$importance[2,2]
+pcPlotData <- data.frame(pcData$x[,1:4], colData[rownames(pcData$x),])
+
+PCA_group <- ggplot(pcPlotData, aes(x=PC1 , y=PC2 , color=sample))+
+  geom_jitter(alpha=0.6, aes(size = 1))+
+  xlab(paste0("PC1 explained variance = ", varPC1*100, "%"))+
+  ylab(paste0("PC2 explained variance = ", varPC2*100, "%"))+
+  scale_color_nejm()+
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  guides(col = guide_legend(ncol = 8))
+
+PCA_stim <- ggplot(pcPlotData, aes(x=PC1 , y=PC2 , color=stimulation))+
+  geom_jitter(alpha=0.6, aes(size=1))+
+  xlab(paste0("PC1 explained variance = ", varPC1*100, "%"))+
+  ylab(paste0("PC2 explained variance = ", varPC2*100, "%"))+
+  scale_color_nejm()+
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  guides(col = guide_legend(ncol = 8))
+
+
+tiff('G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/PCA.tiff', units = "in", height = 5, width = 15, res = 300)
+grid.arrange(PCA_group, PCA_stim, nrow=1)
+dev.off()
+
+
+#select the sample names of 2 different stimulations
+colData_stim2_4_names <- rownames(colData)[which(colData$stimulation == 'redBDOctrl' | colData$stimulation == 'redBDOIL15IL21IFNb')]
+#select the read counts of only the 2 selected stimulations
+stim2_4_countTable <- mRNA_data[, colData_stim2_4_names]
+# make a colData with only the 2 selected stimulations
+stim2_4_colData <- colData[colData_stim2_4_names,]
+
+# Make input data into right format for DESeq2 
+dds_stim_2_4 <- DESeqDataSetFromMatrix(countData = stim2_4_countTable, colData = stim2_4_colData, design = ~ stimulation)
+# perform normalization
+vst_stim_2_4 <- assay(vst(dds_stim_2_4, blind = F))
+# Perform DE analyis
+dds_DE_stim_2_4 <- DESeq(dds_stim_2_4)
+# Extract results from DE analysis
+DE_results <- as.data.frame(results(dds_DE_stim_2_4))
+
+####### Create control genese for TFBSA ####
+sub_res <- rownames(DE_results[order(DE_results$padj, decreasing = T),])[1:69]
+write.csv(sub_res, "G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Data/control_Genes.csv")
+# save DE results
+write.csv(DE_results[,c(2,6)], "G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/DE_results_all_genes.csv")
+
+
+# Make stimulation column into factor data
+stim2_4_colData$stimulation <- factor(stim2_4_colData$stimulation)
+# select DE genes with a adjusted-P-value of less then 0.05
+DE_stim_2_4 <- DE_results[which(DE_results$padj <= 0.05),]
+# show number of DE genes
+nrow(DE_stim_2_4)
+
+# Sort genes based on adjusted-p-val
+sorted_DE_stim_2_4 <- DE_stim_2_4[order(DE_stim_2_4$padj),]
+# make dataframe of only genecodes, log2FC and adjp
+sub_sorted_DE_stim_2_4 <- as.data.frame(sorted_DE_stim_2_4[,c("log2FoldChange", "padj")])
+# save to csv file in Gdrive
+write.csv(sub_sorted_DE_stim_2_4, 'G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/sorted_DE_stim2_4.csv')
+
+
+
+### creating manhatten plot ####
+#use Ref genome annotation info from biomart
+ensembl  <- useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
+# select the attributes you want
+genemap <- as.data.frame(getBM(attributes = c("ensembl_gene_id", "hgnc_symbol","gene_biotype","chromosome_name","start_position","end_position","strand"),
+                 filters = "ensembl_gene_id",
+                 values = rownames(DE_results),
+                 mart = ensembl ))
+
+# remove duplicate rows
+genemap <- genemap[!duplicated(genemap$ensembl_gene_id),]
+
+# make ensemble codes to rownames
+rownames(genemap) <- genemap$ensembl_gene_id
+genemap$ensembl_gene_id <- NULL
+
+# make list of significant DE genes
+sig_names <- rownames(sub_sorted_DE_stim_2_4)
+
+# merge gene map with DE results to comine padj and log2FC
+genemap <- merge(genemap, DE_results, by= 0)
+# convert character chromosome names to numeric chromosome indicators (x = 23, y = 24)
+genemap$chromosome_name <- ChrNumeric(genemap$chromosome_name)
+#removal of NA's
+genemap <- genemap[complete.cases(genemap),]
+#subset top 7 DE genes
+
+# make vulacano plot to visualize DE genes
+tiff("G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/VulcanoPlot.tiff", units = "in", height = 5, width = 5, res = 300)
+ggplot(genemap, aes(x=log2FoldChange, y= -log10(padj)))+
+  geom_point(aes(color= padj <= 0.05))+
+  geom_hline(yintercept = 0, lwd=1, alpha= 0.6)+
+  geom_vline(xintercept = 0, lwd=1, alpha= 0.6)+
+  scale_color_d3()+
+  ggtitle("Stim2_4")+
+  theme_bw()
+dev.off()
+
+#read in results for transcription factor binding site
+TFBSA_results <- read.table("G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Data/TF binding site/fimo.tsv", sep =  "\t", header = T)
+# read in results of differential expression analysis
+DE_results <- read.table("G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/DE_results_all_genes.csv", row.names = 1, sep = ',', header = T)
+#create a genemap with needed attributes
+genemap <- as.data.frame(getBM(attributes = c("ensembl_gene_id", "hgnc_symbol","gene_biotype","chromosome_name","start_position","end_position","strand"),
+                               filters = "ensembl_gene_id",
+                               values = rownames(DE_results),
+                               mart = ensembl ))
+# separate the chr no, start pos and end pos of TFBSA
+motif_pos <- as.data.frame(t(as.data.frame(apply(TFBSA_results, 1, function(motif){
+  tmp <- strsplit(motif[3], split = ":")
+  tmp2 <- strsplit(tmp$sequence_name[2], split = "-")
+  return(c(tmp$sequence_name[1], tmp2[[1]]))
+}))))
+
+comb <- cbind(TFBSA_results,motif_pos)
+colnames(comb)[11:13] <- c("chromosome_name", "start_pos", "end_pos")
+# convert chr names to numeric values
+comb$chromosome_name <- ChrNumeric(comb$chromosome_name)
+
+#add 350 bp to the start site to make it the start site of the gene instead of promotor region
+comb$start_pos <- as.integer(comb$start_pos)+350
+# merge TFBSA results with genemap info to retrieve genes of interest(GOI)
+test <- merge(genemap, comb, by.x = 'start_position', by.y = 'start_pos')
+#select only unique genes of interest
+GOI <- unique(test$hgnc_symbol)
+
+target <- merge(DE_results, genemap, by.x = 0, by.y = "ensembl_gene_id")
+target$chromosome_name <- ChrNumeric(target$chromosome_name)
+target <- target[complete.cases(target),]
+# create manhatten plot with highlighting genes that were enriched in the TFBSA and write to Gdrive
+tiff("G:/Mijn Drive/FromBigDataToPersonalizedMedicine/Results/ManhattanPlot.tiff", units = 'in', height = 5, width = 10, res = 300 )
+manhattan(target,
+          chr = "chromosome_name",
+          bp = 'start_position', 
+          p= "padj", 
+          snp = 'hgnc_symbol', 
+          suggestiveline = F, 
+          genomewideline = F,
+          highlight = GOI,
+          annotatePval = T)
+dev.off()
+
+
---
+++ Pathway_analysis.R
@@ -0,0 +1,329 @@
+#====From Big Data to Personalized Medicine====
+
+#----PATHWAY ANALYSIS----
+#----1. PREPARATION----
+# These packages are from CRAN
+install.packages('data.table')
+install.packages('ggplot2')
+install.packages('dplyr')
+install.packages("ggsci")
+install.packages("ggrepel")
+
+# These packages are from Bioconductor
+BiocManager::install("clusterProfiler")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("pathview")
+BiocManager::install("topGO")
+BiocManager::install("DOSE", force=TRUE)
+BiocManager::install("DESeq2")
+
+# Library
+library(data.table)
+library(ggplot2)
+library(dplyr)
+library(ggsci)
+library(ggrepel)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(pathview)
+library(topGO)
+library(DOSE)
+library(DESeq2)
+
+set.seed(123)
+
+#Import dataset
+library(readxl)
+Book1 <- read.table("/My Downloads/DE_results_all_genes.csv", sep = ",", header = T)
+View(Book1)
+summary(Book1)
+
+#----2. PREPARE THE DATA----
+# convert HGNC gene symbols to more stable ENTREZ IDs
+entrez <- bitr(Book1$X, fromType = "ENSEMBL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
+head(entrez)
+dim(entrez)
+
+# Convert ENTREZ to text, so that R would process it correctly
+entrez$ENTREZID <- as.character(entrez$ENTREZID)
+
+# Add column with ENTREZ IDs to the table, remove rows where ENTREZ was missing
+Book1 <- merge(Book1, entrez, by.x = 'X', by.y = 'ENSEMBL')
+View(Book1)
+dim(Book1)
+
+# Prepare the data for overrepresentation tests
+# Filter in significant (adjusted P<0.05) results
+# Filter in only genes with larger than than 2-fold expression change 
+Book1_sig <- Book1[which(Book1$padj < 0.05 & abs(Book1$log2FoldChange) > 1), ]
+View(Book1_sig)
+
+# Order gene table by effect size (log2 fold-change), from largest to smallest
+Book1 <- Book1[order(Book1$log2FoldChange, decreasing = T), ]
+
+# Look at the range of fold changes
+plot(Book1$log2FoldChange, xlab = 'Gene', ylab = 'log2(FC)')
+
+# Prepare data for GSEA (vector of log2(FC)'s, named by ENTREZ IDs)
+Book1_gsea <- Book1$log2FoldChange
+names(Book1_gsea) <- Book1$ENTREZID
+
+Book1_gsea <- sort(Book1_gsea, decreasing = T)
+head(Book1_gsea)
+# This vector of gene names corresponds to all genes we tested in the analysis and will be used later as "gene universe"
+
+
+#----3. OVER REPRESENTATION ANALYSIS (ORA)----
+
+#----3.1 RUN KEGG OVER-REPRESENTATION ANALYSIS----
+#In this part we run enrichment test (hypergeometric test) for all differentially expressed genes (FDR<0.05, FC>2).
+#We use all genes tested in the RNA-profiling study as a background set or "gene universe". This list was already constructed in the previous step.
+#We will query for all enrichment results and write out 25 most differentially expressed, not accounting the significance.
+#The default multiple testing is done by Benjamini-Hochberg method, flag pvalueCutoff can be used for filtering only significant results (<0.05). Additionally, qvalueCutoff flag is used to filter results based on another popular method of FDR estimation (Storey q-value).
+#The defaults of the command also apply restrictions on the sizes of gene sets tested in the analysis, those can be seen with command ?enrichKEGG.
+
+KEGG_all <- enrichKEGG(gene = Book1_sig$ENTREZID,
+                       organism = 'hsa',
+                       universe = Book1$ENTREZID,
+                       pvalueCutoff = 1, 
+                       qvalueCutoff = 1)
+View(KEGG_all@result)
+write.table(KEGG_all@result[1:17,], 'KEGG_OR_Table.csv', sep = ',', row.names = F)
+
+
+# show the data structure
+head(str(KEGG_all),25)
+
+# result table
+head(KEGG_all@result)  
+# what ontology was tested
+KEGG_all@ontology  
+# what P-value cutoff was used
+KEGG_all@pvalueCutoff     
+
+#Now we look at the results by printing out 25 first rows of the result table. 
+View(head(KEGG_all@result, 17))
+View(head(KEGG_all@result, 5))
+View(head(KEGG_all@result), )
+
+
+#----3.1.1 VISUALIZE OVERALL KEGG ENRICHMENT ANALYSIS RESULTS----
+input_barplot <- KEGG_all@result
+input_barplot$Description <- factor(input_barplot$Description, levels = rev(as.character(input_barplot$Description)))
+# here we apply the default significance thresholds (Benjamini-Hochberg P<0.05 and Storey Q-value<0.2)
+input_barplot <- input_barplot[input_barplot$p.adjust < 0.05 & input_barplot$qvalue < 0.2, ]
+
+BP17 <- ggplot(input_barplot, aes(x = Description, y = -log10(pvalue), fill = Count)) + geom_bar(stat = 'identity') + 
+  theme_classic() + 
+  coord_flip() + scale_fill_continuous(low = 'lightblue', high = 'salmon')
+
+tiff("./KEGG_barplot_17_cat.tiff", units = "in", height = 10, width = 10, res = 300)
+BP17
+dev.off()
+
+#Next we visualize five most enriched pathways on the network graph. 
+#Size of the pathway node shows statistical significance and links indicate gene membership in the pathway. 
+#This gives the static graph as a output. 
+#If it is necessary to investigate further, you can use flag fixed = FALSE to see interactive version.
+
+png('~/R/KEGG_cnetplot.png', width = 20, height = 20, res = 400, units = 'in')
+Networkgraph <- cnetplot(KEGG_all, categorySize = "pvalue", showCategory = 5, foldChange = Book1_gsea)
+dev.off()
+Networkgraph
+
+# Interactive version
+png('~/R/KEGG_cnetplot2.png', width = 20, height = 20, res = 400, units = 'in')
+cnetplot(KEGG_all, categorySize = "pvalue", showCategory = 5, foldChange = Book1_gsea, fixed = F)
+dev.off()
+
+
+#----3.1.2 VISUALIZE THE MOST SIGNIFICANTLY ENRICHED KEGG PATHWAY
+#For KEGG pathways it is possible to visualize the location of differentially expressed genes on the pathway, as well as their magnitude of differential expression. 
+#This kind of visualization might help to identify most relevant genes for given condition and generate new research hypotheses.
+# Following command saves the corresponding KEGG graph to your work directory
+
+#For most significant pathway - Influenza A
+pathview(gene.data = Book1_gsea,
+         pathway.id = KEGG_all@result[1, ]$ID,
+         species = "hsa",
+         limit = list(gene = max(abs(Book1_gsea)), cpd = 1), 
+         out.suffix = "most_sig_pathway")
+
+#For NOD-like receptor signaling pathway
+pathview(gene.data = Book1_gsea,
+         pathway.id = KEGG_all@result[4, ]$ID,
+         species = "hsa",
+         limit = list(gene = max(abs(Book1_gsea)), cpd = 1), 
+         out.suffix = "most_sig_pathway")
+
+
+#----3.2 RUN GENE ONTOLOGY OVER-REPRESENTATION ANALYSIS----
+#Next we run the overrepresentation test for Gene Ontologies.
+#For the sake of brevity, we test only the ontologies from Biological Process category. 
+#By setting the ont flag, is also possible to test Molecular Function (MF), Cellular Component (CC) or all three categories combined (ALL).
+#For visualization purposes we apply this time significance thresholds (Benjamini-Hochberg FDR<0.05 and Storey FDR<0.05).
+#We specify that we test the enrichment for the gene sets with sizes between 10-10000 genes (flags minGSSize and maxGSSize)
+
+#GENE-IDs
+GO_all_ID <- enrichGO(gene = Book1_sig$ENTREZID,
+                   universe = Book1$ENTREZID,
+                   OrgDb = 'org.Hs.eg.db',
+                   ont = 'BP',
+                   pvalueCutoff = 0.05, 
+                   qvalueCutoff = 0.05,
+                   minGSSize = 10, 
+                   maxGSSize = 10000)
+
+GO_all_ID <- setReadable(GO_all_ID, OrgDb = org.Hs.eg.db)
+
+View(head(GO_all_ID@result, 17))
+
+GO_all_ID_table <- data.frame(GO_all_ID[1:17,])
+write.table(GO_all_ID_table, file = "~/R/GO_all_table.txt", sep = ",", quote = FALSE, row.names = F)
+
+#Dotplot
+dotplot(GO_all_ID) + ggtitle("Dotplot")
+no_sig <- nrow(GO_all_ID@result[which(GO_all_ID@result$p.adjust < 0.0001 & GO_all_ID@result$qvalue < 0.2),])
+#GENE-SYMBOL
+GO_all_symbol <- enrichGO(gene = Book1_sig$ENTREZID,
+                   universe = Book1$ENTREZID,
+                   OrgDb = 'org.Hs.eg.db',
+                   ont = 'BP',
+                   pvalueCutoff = 0.05, 
+                   qvalueCutoff = 0.05,
+                   minGSSize = 10, 
+                   maxGSSize = 10000, 
+                   readable = TRUE)
+
+View(head(GO_all_symbol@result))
+
+write.table(GO_all_symbol@result[1:17,], 'GO_OR_Table.csv', sep = ',', row.names = F)
+
+#Dotplot
+dotplot(GO_all_symbol, showCategory=no_sig) + ggtitle("Dotplot")
+
+#Heatmap
+heatplot(GO_all_symbol, foldChange=Book1_gsea) + ggtitle("Heatmap")
+
+#Barplot
+input_barplot_GO <- GO_all_ID@result[1:17,]
+input_barplot_GO$Description <- factor(input_barplot_GO$Description, levels = rev(as.character(input_barplot_GO$Description)))
+# here we apply the default significance thresholds (Benjamini-Hochberg P<0.05 and Storey Q-value<0.2)
+input_barplot_GO <- input_barplot_GO[order(input_barplot_GO$p.adjust, decreasing = T),]
+input_barplot_GO <- input_barplot_GO[input_barplot_GO$p.adjust < 0.05 & input_barplot_GO$qvalue < 0.2, ]
+GO17 <- ggplot(input_barplot_GO, aes(x = Description, y = -log10(pvalue), fill = Count)) + geom_bar(stat = 'identity') + 
+  theme_classic() + 
+  coord_flip() + scale_fill_continuous(low = 'lightblue', high = 'salmon')
+
+tiff("./GO_barplot_17_cat.tiff", units = "in", height = 8, width = 8, res = 300)
+GO17
+dev.off()
+
+#----3.2.1 VISUALIZE THE GO ENRICHMENT ANALYSIS RESULTS----
+#To ease the interpretation and see the hierarchical relationships between most enriched GO terms, we visualize those on graph structure. 
+#Square boxes are significantly enriched terms and color depicts the significance (P-value). 
+#We will visualize 15 most significant terms.
+
+#GENE-ID
+png('~/R/GO_plot_ID.png', height = 10, width = 10, units = 'in', res = 400)
+plotGOgraph(GO_all_ID, firstSigNodes = 5, sigForAll = TRUE)
+dev.off()
+
+#GENE-SYMBOL
+png('~/R/GO_plot_symbol.png', height = 10, width = 10, units = 'in', res = 400)
+plotGOgraph(GO_all_symbol, firstSigNodes = 5, sigForAll = TRUE)
+dev.off()
+
+
+#----3.3 RUN DISEASE ONTOLOGY OVER-REPRESENTATION ANALYSIS----
+#Clusterprofiler allows to do also ORA for human diseases, making use of several databases of curated gene-disease relationships. 
+#As our trait of interest is Coeliac disease, it would be interesting to see differentially expressed genes also show enrichment of some human diseases and whether those are could be related with Coeliac disease or any other autoimmune disease.
+
+DO_all <- enrichDO(gene = Book1_sig$ENTREZID,
+                   universe = Book1$ENTREZID,
+                   pvalueCutoff = 1, 
+                   qvalueCutoff = 1,
+                   ont = "DO")
+View(head(DO_all@result, 25))
+
+
+#----4. GENE SET ENRICHMENT ANALYSIS (GSEA)----
+#Next we will use GSEA (Subramanian et al. 2005) on the differential analysis results. 
+#Unlike ORA, GSEA uses all the results from differential expression analysis, not only significant ones.
+
+#----4.1 RUN GSEA for KEGG pathways
+#For the sake of speed and brevity, we use recommended minimal number of 1000 permutations for this analysis.
+#To get more stable and precise results, your could increase the number of permutations to e.g. 10000 in real scientific work. 
+#Also, we test only KEGG pathways which have more than 50 known gene members.
+
+#GSEA_KEGG
+KEGG_GSEA <- gseKEGG(geneList = Book1_gsea,
+                     organism = 'hsa',
+                     nPerm = 10000,
+                     minGSSize = 50,
+                     pvalueCutoff = 1,
+                     verbose = FALSE)
+
+View(head(KEGG_GSEA@result, 1000))
+View(KEGG_GSEA)
+
+write.table(KEGG_GSEA@result[1:5,], 'KEGG_GSEA_Table.csv', sep = ',', row.names = F)
+
+
+#GSEA_GO
+GO_GSEA <- gseGO(geneList = Book1_gsea,
+                 ont = 'BP', 
+                 OrgDb = org.Hs.eg.db,
+                 nPerm = 10000,
+                 keyType = 'ENTREZID',
+                 minGSSize = 50,
+                 pvalueCutoff = 1,
+                 verbose = FALSE)
+
+View(GO_GSEA@result)
+
+write.table(GO_GSEA@result[1:5,], 'GO_GSEA_Table.csv', sep = ',', row.names = F)
+
+
+#----4.1.1 VISUALIZE GSEA PLOT FOR MOST SIGNIFICANT KEGG PATHWAY----
+
+png('~/R/GSEA_plot.png', height = 6, width = 9, units = 'in', res = 400)
+
+#Here the geneSetID we specify the first (most significant) GSEA result to visualize:
+gseaplot(KEGG_GSEA, geneSetID = head(KEGG_GSEA@result, 1)$ID)
+dev.off()
+
+#GSEA result of NOD-like receptor signaling pathway
+png('~/R/GSEA_plot_NOD.png', height = 6, width = 9, units = 'in', res = 400)
+gseaplot(KEGG_GSEA, geneSetID = "hsa04621")
+dev.off()
+
+GSEA_NOD <- gseaplot(KEGG_GSEA, geneSetID = "hsa04621")
+GSEA_NOD
+
+#leading edge analysis
+library(DOSE)
+
+#Visualize first 5 KEGG pathways in cnetplot
+KEGG_GSEA2 <- setReadable(KEGG_GSEA, OrgDb = org.Hs.eg.db, keyType = 'ENTREZID')
+View(head(KEGG_GSEA2))
+x <- cnetplot(KEGG_GSEA2, categorySize="pvalue", foldChange=Book1_gsea, showCategory = KEGG_GSEA$Description[3])
+x
+
+x2 <- cnetplot(KEGG_GSEA2, categorySize="pvalue", foldChange=Book1_gsea, showCategory = 5, colorEdge = TRUE, layout = 'gem', cex_label_category = 0.5, cex_label_gene = 0.5)
+x2
+
+tiff("./KEGG_cnetplot_5_cat.tiff", units = "in", height = 10, width = 10, res = 300)
+x2
+dev.off()
+
+#Visualize first 5 GO pathways in cnetplot
+GO_GSEA2 <- setReadable(GO_GSEA, OrgDb = org.Hs.eg.db, keyType = 'ENTREZID')
+View(head(GO_GSEA2))
+
+GO2 <- cnetplot(GO_GSEA2, categorySize="pvalue", foldChange=Book1_gsea, showCategory = 5, colorEdge = TRUE, layout = 'gem', cex_label_category = 0.8, cex_label_gene = 0.8)
+GO2
+
+tiff("./GO_cnetplot_5_cat.tiff", units = "in", height = 15, width = 15, res = 300)
+GO2
+dev.off()
